f091b7acbc7ce23631ac5be14ce86217924ef8a4
andyphancode <andyphanpxh@gmail.com>
2023-08-14 19:44:49 -0700

    removed console logs

--- dog-finder/src/DogDetails.js
+++ dog-finder/src/DogDetails.js
@@ -4,14 +4,11 @@ import { useParams } from 'react-router-dom';
 
 function DogDetails({dogs}) {
     const {name} = useParams();
-    console.log(name);
-    console.log(dogs);
+
     if (name) {
-        dogs.forEach((dog) => {
-            console.log(dog.name.toLowerCase());
-            console.log(name);
-        });
+
         const filteredDog = dogs.filter(d => d.name.toLowerCase() === name)[0];
+
         return (
             <div className="DogDetails">
                 <a href={`/dogs/${filteredDog.name}`}>
@@ -25,8 +22,7 @@ function DogDetails({dogs}) {
                 </ul>   
                 <a href="/dogs">Go back</a>
             </div>
-            
-        )
+        );
     }
 
     return null;
